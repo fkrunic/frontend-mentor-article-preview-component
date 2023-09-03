d57e5aee46a55f729039e4b132388492fc02168f
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 15:02:57 -0700

    moved social media to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import SocialMedia from './components/SocialMedia.vue'
 </script>
 
 <template>
@@ -33,28 +34,7 @@
           felt slightly bare and uninviting. I've got some simple tips to help 
           you make any room feel complete.
         </p>
-
-        <!-- Author -->
-        <div class="flex flex-row items-center pt-4 justify-between">
-
-          <!-- Avatar Section -->
-          <div class="flex flex-row gap-4 items-center">
-            <img class="w-10 h-10 rounded-full" src="./assets/avatar-michelle.jpg" alt="michelle">
-            <div class="flex flex-col">
-              <p class="font-bold text-xs text-very-dark-gray-blue">
-                Michelle Appleton
-              </p>
-              <p class="text-xs text-grayish-blue">
-                28 Jun 2020
-              </p>
-            </div>
-          </div>
-
-          <!-- Share Icon -->
-          <div class="flex items-center justify-center w-8 h-8 rounded-full p-2 bg-grayish-blue/25">
-            <img src="./assets/icon-share.svg" alt="share">
-          </div>      
-        </div>
+        <SocialMedia></SocialMedia>
       </div>
     </div>
   </div>
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <!-- Author -->
+    <div class="flex flex-row items-center pt-4 justify-between">
+
+        <!-- Avatar Section -->
+        <div class="flex flex-row gap-4 items-center">
+            <img class="w-10 h-10 rounded-full" src="../assets/avatar-michelle.jpg" alt="michelle">
+            <div class="flex flex-col">
+                <p class="font-bold text-xs text-very-dark-gray-blue">
+                    Michelle Appleton
+                </p>
+                <p class="text-xs text-grayish-blue">
+                    28 Jun 2020
+                </p>
+            </div>
+        </div>
+
+        <!-- Share Icon -->
+        <div class="flex items-center justify-center w-8 h-8 rounded-full p-2 bg-grayish-blue/25">
+            <img src="../assets/icon-share.svg" alt="share">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+}
+</style>
