c6b4e954dc8bbfe1f33475f01641e881b872d347
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 21:31:17 -0700

    tweaked title opacity on avatar

--- src/components/common/Avatar.vue
+++ src/components/common/Avatar.vue
@@ -5,7 +5,7 @@
     <div class="flex flex-row items-center gap-4 mb-4">
         <img class="w-10 h-10 rounded-full" src="../../assets/avatar-michelle.jpg" alt="michelle">
         <div class="flex flex-col gap-1">
-            <p class="font-bold text-xs text-very-dark-gray-blue">Michelle Appleton</p>
+            <p class="font-bold text-xs text-very-dark-gray-blue/90">Michelle Appleton</p>
             <p class="text-xs text-grayish-blue">28 Jun 2020</p>
         </div>
     </div>    
