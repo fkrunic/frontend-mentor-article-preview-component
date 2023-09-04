e1962b4e41b2fdcdbcb83b80abee850e77988456
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 21:08:37 -0700

    refactored inline-social-media into mobile

--- src/components/mobile/Mobile.vue
+++ src/components/mobile/Mobile.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
-import InlineSocialMedia from './InlineSocialMedia.vue'
+import { ref } from 'vue';
+import ShareIcon from '../common/ShareIcon.vue'
+import Avatar from '../common/Avatar.vue'
 defineProps<{ callout: string, bodyText: string }>()
+
+const showShareMenu = ref(false)
+
+const handleClick = (): void => {
+    showShareMenu.value = !showShareMenu.value
+}
 </script>
 
 <template>
@@ -10,8 +18,36 @@ defineProps<{ callout: string, bodyText: string }>()
             <div class="flex flex-col pt-8 gap-4">
                 <p class="px-8 font-bold text-very-dark-gray-blue">{{ callout }}</p>
                 <p class="px-8 pr-6 text-xs leading-5 text-grayish-blue">{{ bodyText }}</p>
-                <InlineSocialMedia></InlineSocialMedia>
-            </div>
+
+                <div class="
+                    flex 
+                    flex-row 
+                    items-center 
+                    justify-between 
+
+                    h-16
+                    p-8
+                    
+                    rounded-b-xl
+                    "
+                    :class="showShareMenu ? 'bg-very-dark-gray-blue' : ''"
+                    >
+
+                    <!-- Social Media -->
+                    <div :class="showShareMenu ? '' : 'hidden'">
+                        <div class="flex flex-row items-center justify-center gap-4">
+                            <p class="text-grayish-blue/75 text-xs tracking-[0.5rem]">SHARE</p>
+                            <img class="hover:cursor-pointer" src="../../assets/icon-facebook.svg" alt="share-facebook">
+                            <img class="hover:cursor-pointer" src="../../assets/icon-twitter.svg" alt="share-twitter">                
+                            <img class="hover:cursor-pointer" src="../../assets/icon-pinterest.svg" alt="share-pinterest">            
+                        </div>
+                    </div>
+
+                    <Avatar :class="showShareMenu ? 'hidden': ''"></Avatar>
+                    <ShareIcon @click="handleClick"></ShareIcon>
+                </div>
+
+            </div>            
         </div>
     </div>
 </template>
