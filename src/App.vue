ae04b0a7d37dfdb1ed8856abaea5820a0e6a6104
Filip Krunic <fkrunic9@gmail.com>
2023-09-03 20:30:57 -0700

    parametrized mobile card

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import Mobile from './components/mobile/Mobile.vue'
+
+const content = {
+  callout: 'Shift the overall look and feel by adding these wonderful touches to furniture in your home',
+  bodyText: [
+    'Ever been in a room and felt like something was missing?',
+    'Perhaps it felt slightly bare and uninviting.',
+    "I've got some simple tips to help you make any room feel complete."
+  ].join(' ')
+}
 </script>
 
 <template>
-  <Mobile class="desktop:hidden"></Mobile>
+  <Mobile 
+    class="desktop:hidden"
+    :callout="content.callout" 
+    :body-text="content.bodyText"
+    ></Mobile>
 </template>
 
 <script lang="ts">
--- src/components/mobile/Mobile.vue
+++ src/components/mobile/Mobile.vue
@@ -1,33 +1,23 @@
 <script setup lang="ts">
 import InlineSocialMedia from './InlineSocialMedia.vue'
+defineProps<{ callout: string, bodyText: string }>()
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-
-    <!-- Card -->
-    <div class="flex flex-col m-6 rounded-xl shadow-xl bg-white">
-        <img class="rounded-t-xl" src="../../assets/drawers-mobile.jpg" alt="drawers">   
-
-        <!-- Callout -->
-        <div class="flex flex-col pt-8 gap-4">
-            <p class="px-8 font-bold text-very-dark-gray-blue">
-                Shift the overall look and feel by adding these wonderful touches 
-                to furniture in your home
-            </p>
-            <p class="px-8 pr-6 text-xs leading-5 text-grayish-blue">
-                Ever been in a room and felt like something was missing? Perhaps it
-                felt slightly bare and uninviting. I've got some simple tips to help 
-                you make any room feel complete.
-            </p>
-        <InlineSocialMedia></InlineSocialMedia>
-      </div>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="flex flex-col m-6 rounded-xl shadow-xl bg-white">
+            <img class="rounded-t-xl" src="../../assets/drawers-mobile.jpg" alt="drawers">
+            <div class="flex flex-col pt-8 gap-4">
+                <p class="px-8 font-bold text-very-dark-gray-blue">{{ callout }}</p>
+                <p class="px-8 pr-6 text-xs leading-5 text-grayish-blue">{{ bodyText }}</p>
+                <InlineSocialMedia></InlineSocialMedia>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 </style>
